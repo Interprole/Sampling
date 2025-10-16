968db812cf11b2c7b70dd8cb67d1be23edc48b10
Veronika Tsareva <vveronikatsareva@yandex.ru>
2025-10-16 10:42:58 +0300

    added filtering results from api endpoint

--- static/scripts.js
+++ static/scripts.js
@@ -4,20 +4,62 @@ async function getJsonData(url) {
     return javascriptObject.results;
 };
 
+// Filtering results from api-endpoint
+function processResultsWithChildren(data, params) {
+    const searchTerm = (params.term || '').toLowerCase().trim();
+    if (searchTerm === '') {
+        const results = data.map(feature => ({
+            text: `${feature.code}_${feature.name}`,
+            children: feature.values.map(value => ({
+                id: value.code,
+                text: `${feature.code}_${feature.name} | ${value.name}`
+            }))
+        }));
+        return {
+            results: results,
+            pagination: {
+                more: false
+            }
+        };
+    }
+    const filteredResults = [];
+    data.forEach(feature => {
+        const filteredChildren = feature.values.filter(value =>
+            `${feature.code}_${feature.name} | ${value.name}`.toLowerCase().includes(searchTerm)
+        );
+        if (filteredChildren.length > 0) {
+            const mappedChildren = filteredChildren.map(value => ({
+                id: value.code,
+                text: `${feature.code}_${feature.name} | ${value.name}`
+            }));
+            filteredResults.push({
+                text: `${feature.code}_${feature.name}`,
+                children: mappedChildren
+            });
+        }
+    });
+    return {
+        results: filteredResults,
+        pagination: {
+            more: false
+        }
+    };
+};
+
 $(document).ready(function() {
     // Tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
-        container: 'body'
-    }));
-
-    // Setting options for Select2 from json
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(
+        tooltipTriggerEl, {
+            container: 'body'
+        }));
+    
+    // Setting options for Select2 from api-endpoint json
     $('.js-example-basic-single, .js-example-basic-multiple').select2({
         width: '100%',
         theme: "classic",
         closeOnSelect: false,
     });
-
     getJsonData("static/documentLanguages.json")
         .then(data =>
             $('.doc-lang-js').select2({
@@ -27,7 +69,6 @@ $(document).ready(function() {
                 closeOnSelect: false
             })
         );
-
     getJsonData("static/languages.json")
         .then(data =>
             $('.include-lang-js, .exclude-lang-js').select2({
@@ -37,24 +78,36 @@ $(document).ready(function() {
                 closeOnSelect: false
             })
         );
-
-    getJsonData("static/grambankFeatures.json")
-        .then(data =>
-            $('.grambank-js').select2({
-                width: '100%',
-                theme: "classic",
-                data: data,
-                closeOnSelect: false
-            })
-        );
-
-    getJsonData("static/walsFeatures.json")
-        .then(data =>
-            $('.wals-js').select2({
-                width: '100%',
-                theme: "classic",
-                data: data,
-                closeOnSelect: false
-            })
-        );
+    $('.grambank-js').select2({
+        width: '100%',
+        theme: "classic",
+        closeOnSelect: false,
+        ajax: {
+            url: '/api/features/grambank',
+            dataType: "json",
+            data: function(params) {
+                return {
+                    q: params.term,
+                    page: params.page
+                };
+            },
+            processResults: processResultsWithChildren
+        }
+    });
+    $('.wals-js').select2({
+        width: '100%',
+        theme: "classic",
+        closeOnSelect: false,
+        data: function(params) {
+            return {
+                q: params.term,
+                page: params.page
+            };
+        },
+        ajax: {
+            url: '/api/features/wals',
+            dataType: "json",
+            processResults: processResultsWithChildren
+        }
+    });
 });
